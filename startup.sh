bf7a1cd029e4fbb1ab180eedc49c014c37669504
Alim Akbiev <akbievalim0578@gmail.com>
2025-03-10 01:13:40 +0300

    feat: add bash script for starting project

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-docker compose up --build
+# Сборка и запуск
+docker compose up --build -d
+
+# Создание миграций
 docker compose exec web python manage.py migrate
 
+# Загрузка тестовых данных
+docker compose exec web python manage.py loaddata test_data.json
+
+# Вызов celery задачи для парсинга вакансий
+docker compose exec web celery -A HHanalyzer call skills_stats.tasks.fetch_vacancies_task
