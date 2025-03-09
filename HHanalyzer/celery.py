85c9d17d1aa69a1b553fac2a5c927477b46aeef1
Alim Akbiev <akbievalim0578@gmail.com>
2025-03-09 16:36:51 +0300

    feat: add celery-beat schedule

--- HHanalyzer/celery.py
+++ HHanalyzer/celery.py
@@ -1,8 +1,17 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HHanalyzer.settings')
 
 app = Celery('HHanalyzer')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'fetch_vacancies': {
+        'task': 'skills_stats.tasks.fetch_vacancies_task',
+        'schedule': crontab(minute='*/10'),
+    },
+}
