6b63236cf45397b46d8600aefe388caeb6f46b55
Alim Akbiev <akbievalim0578@gmail.com>
2025-03-10 01:11:31 +0300

    feat: add the view for getting skills

---
+++ skills_stats/api/v1/views.py
@@ -0,0 +1,16 @@
+from django.db.models import Count
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from skills_stats.models import VacancyKeywords
+
+
+class KeySkillsCountView(APIView):
+    def get(self, request, profession_id):
+        return Response(
+            VacancyKeywords.objects
+            .filter(vacancies__profession_id=profession_id)
+            .annotate(count=Count('vacancies'))
+            .order_by('-count')
+            .values('name', 'count')
+        )
