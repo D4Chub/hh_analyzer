94b9459fb2857847c42204ccf3d2cf739a7606b2
Alim Akbiev <akbievalim0578@gmail.com>
2025-03-10 01:33:44 +0300

    fix: add 'id' field in response

--- skills_stats/api/v1/views.py
+++ skills_stats/api/v1/views.py
@@ -12,5 +12,5 @@ class KeySkillsCountView(APIView):
             .filter(vacancies__profession_id=profession_id)
             .annotate(count=Count('vacancies'))
             .order_by('-count')
-            .values('name', 'count')
+            .values('id', 'name', 'count')
         )
