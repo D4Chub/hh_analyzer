77814afecf104a6738481583e4d6d854faf02c67
Alim Akbiev <akbievalim0578@gmail.com>
2025-03-12 03:52:45 +0300

    fix: change task calling

--- skills_stats/tasks.py
+++ skills_stats/tasks.py
@@ -3,7 +3,7 @@ import logging
 from celery import shared_task
 
 from HHanalyzer.settings import FORMAT
-from skills_stats.api.services.hh_parser import fetch_vacancies_for_profession, process_vacancies
+from skills_stats.api.services.hh_parser import HHParser
 from skills_stats.models import Profession
 
 logger = logging.getLogger(__name__)
@@ -15,7 +15,9 @@ def fetch_vacancies_task():
     professions = Profession.objects.all()
     for profession in professions:
         logging.info(f"Обрабатываем профессию: {profession.name}")
-        vacancies = fetch_vacancies_for_profession(profession)
-        process_vacancies(profession, vacancies)
 
-    logging.info("Обновление вакансий завершено")
+        parser = HHParser(profession)
+        vacancy_ids = parser.fetch_vacancies_ids()
+        parser.fetch_vacancy_details(vacancy_ids)
+
+    logging.info("Обновление вакансий ЗАВЕРШЕНО")
